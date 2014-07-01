34d80fe2bf67a03d45bb629e5f9827733c909837
bikeshtricon <bikesh@triconinfotech.com>
2014-07-01 15:58:27 +0530

    Description: deleted admin app and created hr

--- pypaywheel/settings.py
+++ pypaywheel/settings.py
@@ -30,13 +30,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
+    'django.contrib.hr',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'admin'
+    'hr'
 )
 
 MIDDLEWARE_CLASSES = (
